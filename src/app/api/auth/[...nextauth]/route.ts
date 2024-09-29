a22e8f395861ce99eaaefbf2291cbbae75fa3e1a
Vince Erkadoo <erkadoovince@gmail.com>
2024-09-29 12:23:34 +0400

    must try

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -45,4 +45,6 @@ export const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
 });
 
-export { handler as GET, handler as POST };
+
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
